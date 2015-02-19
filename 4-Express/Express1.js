7c4a6626837ad267ae10a1f71a2c0e8683cfe633
AnasetHamza <anascooby@hotmail.fr>
2015-02-19 15:51:05 +0000

    Update Express1.js

--- 4-Express/Express1.js
+++ 4-Express/Express1.js
@@ -1,12 +1,7 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-		res.writeHead(200);
-		res.end('Salut tout le monde !');
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Vous êtes à l\'accueil');
 });
-
-server.on('close', function() { // On écoute l'évènement close
-console.log('Bye bye !');
-})
-
-server.listen(8080); // Démarre le serveur
-server.close(); // Arrête le serveur. Déclenche l'évènement close
+app.listen(8080);
